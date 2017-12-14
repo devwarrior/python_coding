7eb1e8a239553a8cd73ddde913aae0b59871177d
Administrator <luigi.dandrea@gmail.com>
2017-12-14 06:36:55 +0100

    commented

--- data_structures/lists_and_comprehension.py
+++ data_structures/lists_and_comprehension.py
@@ -1,8 +1,14 @@
 ''' using list with python '''
 LISTA = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(LISTA)
+# append elements to a list
+LISTA.append(8)
+LISTA.append(8)
+print(LISTA)
 
-
-
+# remove elements from a list
+# remove first occurence of 8 in lista
+LISTA.remove(8)
 print(LISTA)
 
 # reverse the list
@@ -11,7 +17,7 @@ print(REV)
 
 # insert element in the middle of the list
 LENGTH = len(LISTA)
-POS = LENGTH / 2
+POS = int(LENGTH / 2)
 LISTA.insert(POS, 100)
 print(LISTA)
 
@@ -51,16 +57,17 @@ for elem in LIST_COMP_2:
         LIST_COMP_3.append(elem)
 print(LIST_COMP_3)
 
-#the same thing using list comprehension
+#the same list using list comprehension
 LIST_COMP_4 = [x for x in LIST_COMP_2 if x%2 == 0]
 print(LIST_COMP_4)
 
 
 # last example all elements contained in both LIST_COMP_2 and LIST_COMP_1
-#  that are even
+# that are even
 LIST_COMP_5 = [x for x in LIST_COMP_2 if x in LIST_COMP_1 and x%2 == 0]
 print(LIST_COMP_5)
 
+# the same list without using list comprehension
 LIST_6 = []
 for elem in LIST_COMP_2:
     if elem in LIST_COMP_1:
